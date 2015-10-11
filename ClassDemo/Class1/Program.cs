cdd54c40e309a767f6ae19198bf7e5b81afc19b4
Lin Jie <ljlovemichael@hotmail.com>
2015-10-11 21:55:15 +0800

    创建项目

---
+++ ClassDemo/Class1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            float b = 1.1f;
+            float c = 1.2f;
+            double d = 1.11;
+            double e = 1.111d;
+            a = (int)b;
+            b = a;
+            d = b;
+            b = (float)d;
+            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}",a,b,c,d,e);
+            Console.ReadLine();
+
+        }
+    }
+}
