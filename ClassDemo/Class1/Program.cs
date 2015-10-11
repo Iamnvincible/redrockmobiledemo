889076eb30c0c7bf6dfcbaf82a666b27e0b68dc9
Lin Jie <ljlovemichael@hotmail.com>
2015-10-11 22:04:00 +0800

    基本数据类型

--- ClassDemo/Class1/Program.cs
+++ ClassDemo/Class1/Program.cs
@@ -15,11 +15,13 @@ namespace Class1
             float c = 1.2f;
             double d = 1.11;
             double e = 1.111d;
+            long f = 12345;
+            long g = 12345L;
             a = (int)b;
             b = a;
             d = b;
             b = (float)d;
-            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}",a,b,c,d,e);
+            Console.WriteLine("{0}--{1}--{2}--{3}--{4}--{5}--{6}",a,b,c,d,e,f,g);
             Console.ReadLine();
 
         }
