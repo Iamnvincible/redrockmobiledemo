e45951dfe62d17507f1174e9d5f2bb964679b1bc
Lin Jie <ljlovemichael@hotmail.com>
2015-10-12 16:34:34 +0800

    string引用

--- ClassDemo/Class1/Program.cs
+++ ClassDemo/Class1/Program.cs
@@ -10,6 +10,7 @@ namespace Class1
     {
         static void Main(string[] args)
         {
+            #region 类型转换，值类型
             int a = 1;
             float b = 1.1f;
             float c = 1.2f;
@@ -21,7 +22,21 @@ namespace Class1
             b = a;
             d = b;
             b = (float)d;
-            Console.WriteLine("{0}--{1}--{2}--{3}--{4}--{5}--{6}",a,b,c,d,e,f,g);
+            Console.WriteLine("{0}--{1}--{2}--{3}--{4}--{5}--{6}", a, b, c, d, e, f, g);
+            Console.ReadLine();
+            #endregion
+
+            #region string 引用
+            string s1 = "a string";
+            string s2 = s1;
+            Console.WriteLine("s1 is " + s1);
+            Console.WriteLine("s2 is " + s2);
+
+            s1 = "another string";
+            Console.WriteLine("s1 is " + s1);
+            Console.WriteLine("s2 is " + s2);
+            #endregion
+
             Console.ReadLine();
 
         }
