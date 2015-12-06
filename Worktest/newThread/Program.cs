6f5e44a3a42423cb2b7a5ec15accb2279c252cd2
Lin Jie <ljlovemichael@hotmail.com>
2015-12-06 13:11:10 +0800

    线程作业解答

---
+++ Worktest/newThread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace newThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Thread t = new Thread(newthread);
+            t.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("主线程2秒");
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine("finished");
+            Console.ReadKey();
+
+        }
+        static void newthread()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("新线程4秒");
+                Thread.Sleep(4000);
+            }
+        }
+    }
+}
